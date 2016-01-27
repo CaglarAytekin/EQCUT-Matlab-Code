3b497d16b5de21da4395c9bd451f69aee74ee900
CaglarAytekin <cagosmail@gmail.com>
2016-01-27 13:23:17 +0200

    bugfix
    Smoothnes Bug has been fixed

--- QCUT.m
+++ QCUT.m
@@ -1,12 +1,12 @@
 function eigvec_now_2D=QCUT(H,smooth,PixNum, LabelLine,width, height)
 [eigvec eigval]=eigs(sparse(H),1,'SM');
-eigvec14=abs(eigvec(:,1));
+eigvec=abs(eigvec(:,1));
 
 if smooth==0
     eigvec=eigvec.^2;
 end
 
-eigvec=mat2gray(eigvec14);
+eigvec=mat2gray(eigvec);
 
 eigvec_now_2D = sup2pixel( PixNum, LabelLine, eigvec );
 eigvec_now_2D = reshape( eigvec_now_2D,width, height  );
